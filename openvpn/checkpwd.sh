23551f55391f9752711c87d058a4e1063ac64a7c
eamlinux <eamlinux@gmail.com>
2021-03-25 09:19:59 +0800

    Create checkpwd.sh

---
+++ openvpn/checkpwd.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+PASSFILE="/etc/openvpn/pwd-file"
+LOG_FILE="/etc/openvpn/logs/openvpn-password.log"
+TIME_STAMP=`date "+%Y-%m-%d %T"`
+
+readarray -t lines < $1
+username=${lines[0]}
+password=${lines[1]}
+
+if [ ! -r "${PASSFILE}" ]; then
+  echo "${TIME_STAMP}: Could not open password file \"${PASSFILE}\" for reading." >> ${LOG_FILE}
+  exit 1
+fi
+
+CORRECT_PASSWORD=`awk '!/^;/&&!/^#/&&$1=="'${username}'"{print $2;exit}' ${PASSFILE}`
+
+if [ "${CORRECT_PASSWORD}" = "" ]; then
+  echo "${TIME_STAMP}: User does not exist: username=\"${username}\", password=\"${password}\"." >> ${LOG_FILE}
+  exit 1
+fi
+
+if [ "${password}" = "${CORRECT_PASSWORD}" ]; then
+  echo "${TIME_STAMP}: Successful authentication: username=\"${username}\"." >> ${LOG_FILE}
+  exit 0
+fi
+
+echo "${TIME_STAMP}: Incorrect password: username=\"${username}\", password=\"${password}\"." >> ${LOG_FILE}
+exit 1
