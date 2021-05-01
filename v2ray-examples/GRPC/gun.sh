b087a61a0857d2abb63bd91cb9e6f2c0e2e74659
eamlinux <eamlinux@gmail.com>
2021-05-01 12:28:56 +0800

    Create gun.sh

---
+++ v2ray-examples/GRPC/gun.sh
@@ -0,0 +1,49 @@
+sudo tee /opt/v2ray/gun.json > /dev/null <<EOF
+{
+  "log": {"loglevel": "none"},
+  "inbounds": [{
+    "sniffing": {
+      "enabled": true,
+      "destOverride": ["http","tls"]
+    },
+    "protocol": "vless",
+    "listen": "127.0.0.1",
+    "port": "10086",
+    "tag": "vless-gun",
+    "settings": {
+      "decryption":"none",
+      "clients": [
+        {"id": "$(v2ctl uuid)"}
+      ]
+    },
+    "streamSettings": {
+      "network": "gun",
+      "security": "none",
+      "grpcSettings": {
+        "serviceName": "GunService"
+      }
+    }
+  }],
+  "outbounds": [{
+    "protocol": "freedom",
+    "settings": {},
+    "tag": "direct"
+  },{
+    "protocol": "blackhole",
+    "settings": {},
+    "tag": "block"
+  }],
+  "routing": {
+    "domainStrategy": "AsIs",
+    "rules": [{
+      "type": "field",
+      "inboundTag": ["vless-gun"],
+      "outboundTag": "direct"
+    },{
+      "type": "field",
+      "outboundTag": "block",
+      "protocol": ["bittorrent"]
+    }]
+  }
+}
+EOF
